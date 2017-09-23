002706f45868d16a6fc6e85bcb5c745280bfb296
bimbodado <dhlinh98@gmail.com>
2017-09-22 20:11:02 -0400

    first commit

---
+++ src/PointCP5.java
@@ -0,0 +1,15 @@
+public abstract class PointCP5
+{
+
+  
+
+  
+  //Instance methods **************************************************
+ 
+ 
+  public abstract double getX();
+  public abstract double getY();
+  
+  public abstract double getRho();
+  public abstract double getTheta();
+}
