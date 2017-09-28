9017e6b1760ed8e22a27726db3dea46d9bc525cd
AnthonySabbagh <asabb087@uottawa.ca>
2017-09-28 15:11:33 -0400

    Updated TestTime class

--- src/TestTime.java
+++ src/TestTime.java
@@ -1,9 +1,57 @@
 import java.io.IOException;
+import java.util.Arrays;
 class TestTime{
 	public static void main(String args[]){
+		
+		double current;
+		double[] array= new double[21];
+		double greatest=0;
+		double least=999999999;
+		clearArray(array);
+		test2();
+		for (int i=0; i<array.length; i++){
+			current = test2();
+			least = Math.min(least, current);
+			greatest = Math.max(greatest, current);
+			array[i]=current;
+		}
+		Arrays.sort(array);
+		System.out.println("Design 2: min= " + least + " ms " +
+				"max= " + greatest +" ms " +
+				"median= " + array[10]);
+		
+		greatest=0;
+		least=999999999;
+		clearArray(array);
+		test3();
+		for (int i=0; i<array.length; i++){
+			current = test3();
+			least = Math.min(least, current);
+			greatest = Math.max(greatest, current);
+			array[i]=current;
+		}
+		Arrays.sort(array);
+		System.out.println("Design 3: min= " + least + " ms " +
+				"max= " + greatest +" ms " +
+				"median= " + array[10]);
+		
+		greatest=0;
+		least=999999999;
+		clearArray(array);
+		test5();
+		for (int i=0; i<array.length; i++){
+			current = test5();
+			least = Math.min(least, current);
+			greatest = Math.max(greatest, current);
+			array[i]=current;
+		}
+		Arrays.sort(array);
+		System.out.println("Design 5: min= " + least + " ms " +
+				"max= " + greatest +" ms " +
+				"median= " + array[10]);
+		
 	    byte[] buffer = new byte[1024];  //Buffer to hold byte input
 		String input = "";
-		int i;
 		while (input!="4"){
 			System.out.println("\nEnter design 2,3, or 5, or 4 to quit");
 	        for(int k=0; k<1024; k++)
@@ -15,49 +63,69 @@ class TestTime{
 	        	System.exit(0);
 	        }
 	        input = new String(buffer).trim(); //gather input
-	        double t1 = System.currentTimeMillis();
 	        if (input.charAt(0)=='2'){
-	        	t1=System.nanoTime();
-	        	PointCP2 point;
-	        	for (i=0; i<10000; i++){
-	        		point=new PointCP2(i, 10000-i);
-	        		point.getX();
-	        		point.getY();
-	        		point.getRho();
-	        		point.getTheta();
-	        	}
-	        	System.out.println("Design 2: " + (System.nanoTime()-t1) + "ms");
+	        	System.out.println(test2()+"ms");
 	        } else if (input.charAt(0)=='3'){
-	        	t1=System.currentTimeMillis();
-	        	PointCP3 point;
-	        	for (i=0; i<10000; i++){
-	        		point=new PointCP3(i, 10000-i);
-	        		point.getX();
-	        		point.getY();
-	        		point.getRho();
-	        		point.getTheta();
-	        	}
-	        	System.out.println("Design 3: " + (System.currentTimeMillis()-t1) + "ms");
+	        	System.out.println(test3()+"ms");
 	        } else if (input.charAt(0)=='5'){
-	        	t1=System.currentTimeMillis();
-	        	PointCP5 point;
-	        	for (i=0; i<5000; i++){
-	        		point=new PointCP2(i, 5000-i);
-	        		point.getX();
-	        		point.getY();
-	        		point.getRho();
-	        		point.getTheta();
-	        	}
-	        	for (i=0; i<5000; i++){
-	        		point=new PointCP3(i, 5000-i);
-	        		point.getX();
-	        		point.getY();
-	        		point.getRho();
-	        		point.getTheta();
-	        	}
-	        	System.out.println("Design 5: " + (System.currentTimeMillis()-t1) + "ms");
+	        	System.out.println(test5()+"ms");
 	        }
 	        
 		}
+	}
+	private static double test2(){
+
+    	double t1=System.nanoTime()/1000000.0;
+    	PointCP2 point;
+    	for (int i=0; i<10000000; i++){
+    		point=new PointCP2(i, 10000000-i);
+    		point.getX();
+    		point.getY();
+    		point.getRho();
+    		point.getTheta();
+    		point=null;
+    	}
+    	return System.nanoTime()/1000000.0-t1;
+	}
+	
+	private static double test3(){
+		double t1=System.nanoTime()/1000000.0;
+    	PointCP3 point;
+    	for (int i=0; i<10000000; i++){
+    		point=new PointCP3(i, 10000000-i);
+    		point.getX();
+    		point.getY();
+    		point.getRho();
+    		point.getTheta();
+    		point=null;
+    	}
+    	return System.nanoTime()/1000000.0-t1;
+	}
+	
+	private static double test5(){
+		double t1=System.nanoTime()/1000000.0;
+    	PointCP5 point;
+    	for (int i=0; i<5000000; i++){
+    		point=new PointCP2(i, 5000000-i);
+    		point.getX();
+    		point.getY();
+    		point.getRho();
+    		point.getTheta();
+    		point=null;
+    	}
+    	for (int i=0; i<5000000; i++){
+    		point=new PointCP3(i, 5000000-i);
+    		point.getX();
+    		point.getY();
+    		point.getRho();
+    		point.getTheta();
+    		point=null;
+    	}
+    	return System.nanoTime()/1000000.0-t1;
+	}
+	
+	private static void clearArray(double[] array){
+		for (int i=0; i<array.length; i++)
+			array[i]=0;
 	}
 }
